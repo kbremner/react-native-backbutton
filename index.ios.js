cdbbadf7bcff37538457560d35c75b7cf306ba8b
kbremner <kyle@defining.tech>
2016-10-07 09:08:35 +0100

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,67 @@
+const WEBVIEW_REF = "WEBVIEW_REF";
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  WebView
+} from 'react-native';
+
+class ReactNativeBackButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { canGoBack: false };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.topbar}>
+          <TouchableOpacity
+            disabled={!this.state.canGoBack}
+            onPress={this.onBack.bind(this)}
+            >
+            <Text style={this.state.canGoBack ? styles.topbarText : styles.topbarTextDisabled}>Go Back</Text>
+          </TouchableOpacity>
+        </View>
+        <WebView
+          ref={WEBVIEW_REF}
+          style={{flex: 1}}
+          onNavigationStateChange=
+            {this.onNavigationStateChange.bind(this)}
+          source={{uri: 'https://google.com'}}
+          /> 
+      </View>
+    );
+  }
+
+  onBack() {
+    this.refs[WEBVIEW_REF].goBack();
+  }
+
+  onNavigationStateChange(navState) {
+    this.setState({
+      canGoBack: navState.canGoBack
+    });
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15, /* Padding to push below the navigation bar */
+    backgroundColor: '#F5FCFF',
+  },
+  topbar: {
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topbarTextDisabled: {
+    color: 'gray'
+  }
+});
+
+AppRegistry.registerComponent('ReactNativeBackButton', () => ReactNativeBackButton);
